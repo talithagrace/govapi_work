465678a4b93ee20df9c4b861fb824ea659eb5507
talithagrace <talithagrace.brown@gmail.com>
2018-02-11 23:34:42 +0000

    table now shows blank as default

--- blog/blog_views.py
+++ blog/blog_views.py
@@ -3,49 +3,49 @@ from django.shortcuts import render
 #from blog.models import Legislation
 
 def searchlist(request):
-    if request.method == 'GET':
-        search_query = request.GET.get('search_box', None)
-        #create lists
+    search_query = request.GET.get('search_box', None)
+    #create lists
     legislation_1 = []
     legislation_2 = []
     consultations = []
+    if search_query:
+        url_1 = 'http://www.legislation.gov.uk/id'
+        params_1 = {'title': search_query}
+        base_url_1 = 'http://www.legislation.gov.uk'
 
-    url_1 = 'http://www.legislation.gov.uk/id'
-    params_1 = {'title': search_query}
-    base_url_1 = 'http://www.legislation.gov.uk'
-
-    url_2 = 'https://www.gov.uk/government/publications'
-    params_2 = {'keywords': search_query, 'publication_filter_option': 'consultations'}
-    base_url_2 = 'https://www.gov.uk'
+        url_2 = 'https://www.gov.uk/government/publications'
+        params_2 = {'keywords': search_query, 'publication_filter_option': 'consultations'}
+        base_url_2 = 'https://www.gov.uk'
 
 
-    r_1 = requests.get(url_1, params_1)
+        r_1 = requests.get(url_1, params_1)
 
-    r_1.raise_for_status()
+        r_1.raise_for_status()
 
-    search_results_1 = bs4.BeautifulSoup(r_1.content, 'html.parser')
+        search_results_1 = bs4.BeautifulSoup(r_1.content, 'html.parser')
 
-    for link in search_results_1.find_all('a', href=re.compile("/id/ukpga")):
-        #append each link in the loop to the list created above
-        legislation_1.append(base_url_1 + link.get('href'))
+        for link in search_results_1.find_all('a', href=re.compile("/id/ukpga")):
+            #append each link in the loop to the list created above
+            legislation_1.append(base_url_1 + link.get('href'))
 
-    for link in search_results_1.find_all('a', href=re.compile("/id/uksi")):
-        #append each link in the loop to the list created above
-        legislation_2.append(base_url_1 + link.get('href'))
+        for link in search_results_1.find_all('a', href=re.compile("/id/uksi")):
+            #append each link in the loop to the list created above
+            legislation_2.append(base_url_1 + link.get('href'))
 
-    r_2 = requests.get(url_2, params_2)
+        r_2 = requests.get(url_2, params_2)
 
-    r_2.raise_for_status()
+        r_2.raise_for_status()
 
-    search_results_2 = bs4.BeautifulSoup(r_2.content, 'html.parser')
+        search_results_2 = bs4.BeautifulSoup(r_2.content, 'html.parser')
 
-    for link in search_results_2.find_all('a', href=re.compile("/government/consultations")):
-        consultations.append(base_url_2 + link.get('href'))
+        for link in search_results_2.find_all('a', href=re.compile("/government/consultations")):
+            consultations.append(base_url_2 + link.get('href'))
 
     context = {
         'legislation_1': legislation_1,
         'legislation_2': legislation_2,
         'consultations': consultations
-    }
+        }
     return render(request, 'blog/search_list.html', context)
+
 # Create your views here.
