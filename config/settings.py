e7b9fb1ccb18509f086218ecd15c577ae7f92939
talithagrace <talithagrace.brown@gmail.com>
2018-03-02 11:13:12 +0000

    added coverage

--- config/settings.py
+++ config/settings.py
@@ -116,7 +116,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 
-#SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ['SECRET_KEY']
 import dj_database_url
 DATABASES = { 'default': dj_database_url.config(conn_max_age=500) }
 
