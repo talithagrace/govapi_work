8edb9ffd2d21374b4fc69ec749ed12edd35b01bb
talithagrace <talithagrace.brown@gmail.com>
2019-12-13 23:12:47 +0000

    updated django and whitenoise

--- config/wsgi.py
+++ config/wsgi.py
@@ -13,8 +13,5 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
+
 application = get_wsgi_application()
-
-from whitenoise.django import DjangoWhiteNoise
-
-application = DjangoWhiteNoise(application)
